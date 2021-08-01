b4e08be56a948ac76e49a241391b1a692f1bd00f
Mori Shunta <a2139sm@aiit.ac.jp>
2021-08-01 23:57:03 +0900

    gcm add

---
+++ gcm.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#入力していない場合エラー
+if [ ! $2 ] || [ ! $1 ]  ;
+then
+	echo "入力エラー"
+	exit 1
+fi
+a=$1
+b=$2
+#aよりbが大きいなら入れ替え
+if [ $a -lt $b ] ;
+then
+	ch=$a
+	a=$b
+	b=$ch
+fi
+save=$(( $a % $b ))
+while [ $save != 0 ]
+do
+	a=$b
+	b=$save
+	save=$(( $a % $b ))
+done
+echo "$b"
